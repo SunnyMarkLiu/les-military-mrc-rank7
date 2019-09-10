d915fdd2b67c0f3e06b5c91b7d84850a5ece1e40
SunnyMarkLiu <sunnymarkliu101@gmail.com>
2019-09-10 08:52:38 +0800

    clean code

--- TODO.md
+++ TODO.md
@@ -2,6 +2,7 @@
 ## 数据预处理
 - text_cleaning.py 中计算 match score 是否考虑 para ngram ==> para ngram 效果较好，考虑了前后上下文，score=0的与supporting para重合较少，区分度高
 - 统计段落的长度分布，以及答案是否在长段落的情况，决定是否需要对长段落进行拆分
+    - 段落筛选难以保证答案覆盖率
 
 ## bert-baseline相关
 - 训练被截断的例子的处理
--- preprocess/run_4.check_and_split_mrc_dataset.sh
+++ preprocess/run_4.check_and_split_mrc_dataset.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 source_dir="../input/mrc_dataset/"      # text_analysis
-target_dir="../input/final_mrc_dataset/"
+target_dir="../input/aug_mrc_dataset/"
+min_doc_len=600
 max_doc_len=800
 
-nohup cat ${source_dir}split_train_00 |python 4.check_and_split_mrc_dataset.py ${max_doc_len} > ${target_dir}split_train_00 2>&1 &
-nohup cat ${source_dir}split_train_01 |python 4.check_and_split_mrc_dataset.py ${max_doc_len} > ${target_dir}split_train_01 2>&1 &
-nohup cat ${source_dir}split_train_02 |python 4.check_and_split_mrc_dataset.py ${max_doc_len} > ${target_dir}split_train_02 2>&1 &
-nohup cat ${source_dir}split_train_03 |python 4.check_and_split_mrc_dataset.py ${max_doc_len} > ${target_dir}split_train_03 2>&1 &
-nohup cat ${source_dir}split_train_04 |python 4.check_and_split_mrc_dataset.py ${max_doc_len} > ${target_dir}split_train_04 2>&1 &
-nohup cat ${source_dir}split_train_05 |python 4.check_and_split_mrc_dataset.py ${max_doc_len} > ${target_dir}split_train_05 2>&1 &
-nohup cat ${source_dir}split_train_06 |python 4.check_and_split_mrc_dataset.py ${max_doc_len} > ${target_dir}split_train_06 2>&1 &
-nohup cat ${source_dir}split_train_07 |python 4.check_and_split_mrc_dataset.py ${max_doc_len} > ${target_dir}split_train_07 2>&1 &
-nohup cat ${source_dir}split_train_08 |python 4.check_and_split_mrc_dataset.py ${max_doc_len} > ${target_dir}split_train_08 2>&1 &
-nohup cat ${source_dir}split_train_09 |python 4.check_and_split_mrc_dataset.py ${max_doc_len} > ${target_dir}split_train_09 2>&1 &
+nohup cat ${source_dir}split_train_00 |python 4.check_and_split_mrc_dataset.py ${min_doc_len} ${max_doc_len} > ${target_dir}split_train_00 2>&1 &
+nohup cat ${source_dir}split_train_01 |python 4.check_and_split_mrc_dataset.py ${min_doc_len} ${max_doc_len} > ${target_dir}split_train_01 2>&1 &
+nohup cat ${source_dir}split_train_02 |python 4.check_and_split_mrc_dataset.py ${min_doc_len} ${max_doc_len} > ${target_dir}split_train_02 2>&1 &
+nohup cat ${source_dir}split_train_03 |python 4.check_and_split_mrc_dataset.py ${min_doc_len} ${max_doc_len} > ${target_dir}split_train_03 2>&1 &
+nohup cat ${source_dir}split_train_04 |python 4.check_and_split_mrc_dataset.py ${min_doc_len} ${max_doc_len} > ${target_dir}split_train_04 2>&1 &
+nohup cat ${source_dir}split_train_05 |python 4.check_and_split_mrc_dataset.py ${min_doc_len} ${max_doc_len} > ${target_dir}split_train_05 2>&1 &
+nohup cat ${source_dir}split_train_06 |python 4.check_and_split_mrc_dataset.py ${min_doc_len} ${max_doc_len} > ${target_dir}split_train_06 2>&1 &
+nohup cat ${source_dir}split_train_07 |python 4.check_and_split_mrc_dataset.py ${min_doc_len} ${max_doc_len} > ${target_dir}split_train_07 2>&1 &
+nohup cat ${source_dir}split_train_08 |python 4.check_and_split_mrc_dataset.py ${min_doc_len} ${max_doc_len} > ${target_dir}split_train_08 2>&1 &
+nohup cat ${source_dir}split_train_09 |python 4.check_and_split_mrc_dataset.py ${min_doc_len} ${max_doc_len} > ${target_dir}split_train_09 2>&1 &
